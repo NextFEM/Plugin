1c6789cd9d925b1f6b17f25b005a5c29feb75fc7
NextFEM <91146215+NextFEM@users.noreply.github.com>
2021-09-21 20:09:55 +0200

    Plugin code and project files for Visual Studio

---
+++ TrussDrawer.vb
@@ -0,0 +1,58 @@
+﻿Public Class TrussDrawer
+    Friend caller As plugin
+    Dim nf As NextFEMapi.API
+
+    Private Sub TrussDrawer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        nf = caller.API ' short alias for caller.API
+
+        ' loads materials and sections
+        mats.Items.Clear()
+        For Each st In nf.getMaterialsLibrary
+            mats.Items.Add(st)
+        Next
+        sects.Items.Clear()
+        For Each st In nf.getSectionsLibrary
+            sects.Items.Add(st)
+        Next
+    End Sub
+
+    ' clear selection on screen
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        nf.clearSelection()
+        caller.RequestScreenUpdate(caller, False, NextFEMplugin.ViewState.NoOperation) ' do not change view
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        hei.Value = span.Value / 15
+        wid.Value = span.Value / bays.Value
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' call undo
+        caller.RequestUndo(NextFEMplugin.undoOps.Normal)
+        ' Draw structure using APIs
+        Dim w As Double = wid.Value
+        Dim h As Double = hei.Value
+        Dim stand As New List(Of Drawing.Point) ' use this to store node number of vertical trusses
+        For i = 0 To bays.Value
+            stand.Add(New Drawing.Point(nf.addNode(i * w, 0, 0), nf.addNode(i * w, 0, h)))
+        Next
+        ' section and material
+        Dim sect As Integer = If(sects.SelectedItem <> "", nf.addSectFromLib(sects.SelectedItem), 0)
+        Dim mat As Integer = If(mats.SelectedItem <> "", nf.addMatFromLib(mats.SelectedItem), 0)
+        For i = 0 To stand.Count - 1
+            Dim st As Drawing.Point = stand(i)
+            ' vertical trusses
+            nf.addTruss(st.X, st.Y, sect, mat)
+            ' horizontal trusses
+            If i > 0 Then
+                nf.addTruss(st.X, stand(i - 1).X, sect, mat)
+                nf.addTruss(st.Y, stand(i - 1).Y, sect, mat)
+                ' diagonals
+                nf.addTruss(st.X, stand(i - 1).Y, sect, mat)
+            End If
+        Next
+        ' refresh the view
+        caller.RequestScreenUpdate(caller, True, NextFEMplugin.ViewState.NoOperation)
+    End Sub
+End Class
---
+++ plugin.vb
@@ -0,0 +1,43 @@
+﻿Imports NextFEMplugin
+
+Public Class plugin
+    Implements IPlugin
+
+    Public Sub LaunchMe(hnd As IntPtr) Implements IPlugin.LaunchMe
+        Dim frm As New TrussDrawer
+        frm.caller = Me
+        frm.Text = Me.PluginName
+        frm.Show(System.Windows.Forms.Control.FromHandle(hnd))
+    End Sub
+
+    Public ReadOnly Property PluginAuthor As String Implements IPlugin.PluginAuthor
+        Get
+            Return "NextFEM Support Team"
+        End Get
+    End Property
+
+    Public ReadOnly Property PluginName As String Implements IPlugin.PluginName
+        Get
+            Return "Truss Drawer"
+        End Get
+    End Property
+
+    Public ReadOnly Property iconFile As String Implements IPlugin.iconFile
+        Get
+            Return "truss.ico"
+        End Get
+    End Property
+
+    Public Event updateModel(sender As Object, resize As Boolean, Vstate As ViewState) Implements IPlugin.updateModel
+    Friend Sub RequestScreenUpdate(sender As Object, resize As Boolean, Vstate As ViewState)
+        RaiseEvent updateModel(sender, resize, Vstate)
+    End Sub
+    Public Event undoCall(undoProperty As undoOps) Implements IPlugin.undoCall
+    Friend Sub RequestUndo(undoProperty As undoOps)
+        RaiseEvent undoCall(undoProperty)
+    End Sub
+
+    Public Property API As NextFEMapi.API Implements IPlugin.API
+    Public Property currentLoadcase As String Implements IPlugin.currentLoadcase
+    Public Property currentTime As String Implements IPlugin.currentTime
+End Class
